677f14f93c1e274acac05b2f8a657ca52da6f392
gyan-cell <jha07944@gmail.com>
2026-02-09 21:12:33 +0530

    feat  : Some basic questions

--- questions/palindrome.go
+++ questions/palindrome.go
@@ -1,5 +1,28 @@
 package main
 
-func checkPalindrome(str string) bool {
-	return true
+import (
+	"fmt"
+)
+
+func checkPalindrome(str string, len int) bool {
+	strArray := []rune(str)
+	isTrue := true
+	for i, j := 0, len-1; i < j; i, j = i+1, j-1 {
+		if strArray[i] != strArray[j] {
+			isTrue = false
+		}
+	}
+	return isTrue
+}
+
+func main() {
+	var str string
+	fmt.Printf("Plese Enter the string ")
+	fmt.Scanln(&str)
+	result := checkPalindrome(str, len(str))
+	if result {
+		fmt.Printf("Palindrome. \n ")
+	} else {
+		fmt.Println("Not palindrome \n")
+	}
 }
